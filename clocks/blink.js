c591d5b7eb96748f920981bbfbe65b98ddb6b9eb
Jane <jmfessy@mit.edu>
2017-10-10 03:27:16 -0400

    Update blink.js

--- clocks/blink.js
+++ clocks/blink.js
@@ -15,30 +15,45 @@ background(255);
     seconds = date.getSeconds(),
     ms = date.getMilliseconds();
     var hour = ["twelve","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","one","two","three","four","five","six","seven","eight","nine","ten","eleven"];
-	var minute = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty-one","twenty-two","twenty-three","twenty-four","twenty-five","twenty-six","twenty-seven","twenty-eight","twenty-nine","thirty","thirty-one","thirty-two","thirty-three","thirty-four","thirty-five","thirty-six","thirty-seven","thirty-eight","thirty-nine","fourty","fourty-one","fourty-two","fourty-three","fourty-four","fourty-five","fourty-six","fourty-seven","fourty-eight","fourty-nine","fifty","fifty-one","fifty-two","fifty-three","fifty-four","fifty-five","fifty-six","fifty-seven","fifty-eight","fifty-nine"];
-	var second = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty-one","twenty-two","twenty-three","twenty-four","twenty-five","twenty-six","twenty-seven","twenty-eight","twenty-nine","thirty","thirty-one","thirty-two","thirty-three","thirty-four","thirty-five","thirty-six","thirty-seven","thirty-eight","thirty-nine","fourty","fourty-one","fourty-two","fourty-three","fourty-four","fourty-five","fourty-six","fourty-seven","fourty-eight","fourty-nine","fifty","fifty-one","fifty-two","fifty-three","fifty-four","fifty-five","fifty-six","fifty-seven","fifty-eight","fifty-nine"];
-
-if( minutes == 0){
-	fill(255,0,0);
-	textSize(100);
-	text(hour[hours],250, 250)
-}
-
-if( seconds == 0){
-	fill(0)
-	textSize(50);
-	text(minute[minutes],250, 250)
-}
-
-if( ms > 900){
-	fill(0)
-	textSize(20);
-	text(second[seconds],350, 250)
-}
+	var minute = ["zero", "one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty-one","twenty-two","twenty-three","twenty-four","twenty-five","twenty-six","twenty-seven","twenty-eight","twenty-nine","thirty","thirty-one","thirty-two","thirty-three","thirty-four","thirty-five","thirty-six","thirty-seven","thirty-eight","thirty-nine","fourty","fourty-one","fourty-two","fourty-three","fourty-four","fourty-five","fourty-six","fourty-seven","fourty-eight","fourty-nine","fifty","fifty-one","fifty-two","fifty-three","fifty-four","fifty-five","fifty-six","fifty-seven","fifty-eight","fifty-nine", "sixty"];
+	var second = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty-one","twenty-two","twenty-three","twenty-four","twenty-five","twenty-six","twenty-seven","twenty-eight","twenty-nine","thirty","thirty-one","thirty-two","thirty-three","thirty-four","thirty-five","thirty-six","thirty-seven","thirty-eight","thirty-nine","fourty","fourty-one","fourty-two","fourty-three","fourty-four","fourty-five","fourty-six","fourty-seven","fourty-eight","fourty-nine","fifty","fifty-one","fifty-two","fifty-three","fifty-four","fifty-five","fifty-six","fifty-seven","fifty-eight","fifty-nine", "sixty"];
 
 	fill(0,0,255);
 	textSize(10);
-	text(nf(ms,[3],[0]),450, 250)
+	text(nf(ms,[3],[0]),350, 250);
+
+if( ms > 900){
+	fill(0);
+	textSize(20);
+	text(seconds,320, 250);
+	console.log(minutes);
+}
+
+
+if( seconds > 58){
+	fill(255,0,0);
+	textSize(100);
+	text(minutes,200, 250);
+}
+
+if( minutes > 58){
+	fill(255,0,0);
+	textSize(200);
+	text(hours,90, 250)
+}
+/*
+textSize(100);
+	text(minutes,200, 250);
+textSize(200);
+	text(hours,90, 250)
+
+if( seconds > 10){
+	fill(0);
+	textSize(20);
+	text(minute[minutes],350, 250);
+	console.log(minutes);
+}
+*/
 
 
 } 
