5d2a585e976f59003aa006525e1eb9b9ee7b2fd2
Jane <jmfessy@mit.edu>
2017-11-02 23:15:18 -0400

    Add files via upload

--- census/census2.js
+++ census/census2.js
@@ -19,6 +19,8 @@ var xcanvas = 11*120; var ycanvas = 11.4*120; var centerx = xcanvas/2; var cente
 
 //status!
   var status=0;
+  var mr = 0;
+  var cs = 0;
 
 // ------------------------------SETUP---------------------------------
  
@@ -27,7 +29,7 @@ function setup() {
   createCanvas(xcanvas, ycanvas);
   background(color('#152F48'));
   query();
-  noLoop();
+  //noLoop();
 
 }
 
@@ -41,19 +43,13 @@ var county =  01;
 var state = 06;
 
 
-
-// Run the API call
 function query(state, county, race) {
 	loadJSON(url1, gotData, 'json')
 }
 
-// Request is completed
+
 function gotData(data) {
   queryResult = data;
-	//TODO: Figure out how to properly return the hashmap
-  // console.log(data);
-
-  // only look at current results:
 	var currentPopulation = queryResult;
 	console.log(currentPopulation);
 	var whitepop = currentPopulation[1][0];
@@ -78,11 +74,11 @@ function gotData(data) {
 var smlltxtsz = 22;
 
 var aswh = "Asian Whites "; var asbl = "Black Asians "; var ashs = "Asian Hispanics "; var asind = "Indian Asians "; var aspi = "Asian Pacific Islander ";
-var multiracialMatrix = [aswh, asbl, ashs, asind, aspi]
-var mostL = " most likely"; var leastL = " least likely"; var medL = " on average likely";
+var multiracialMatrix = [aswh, asbl, ashs, asind, aspi, "White Blacks ", "White Hispanics ", "White Indians ", "White Pacific Islanders ", "Black Hispanics ", "Black Indians ", "Black Pacific Islanders ", "Hispanic Indians ", "Hispanic Pacific Islanders ","Indian Pacific Islanders "]
+var mostL = " most likely"; var leastL = " least likely"; var medL = " on average likely"; var are = "are";
 var chooseSeverity = [mostL, medL, leastL]
   // other words that are needed
-var are = "are";
+
 
 // colors
 var asiancolor = "#E0231E", hispaniccolor = "#E07A22", blackcolor ="#E0A12E", pacificcolor = "#E0C634", indiancolor = "#2D62BA", whitecolor = "#387BBF";
@@ -96,7 +92,8 @@ var esize = 20, shifted = esize*1.55; var eycanvas = 0.982;
 // ------------------------------SETUP FOR LOOKS---------------------------------
 
 function draw(){
-  pieChart(400, angles);
+  background(color('#152F48'));
+
 
   push();
   noFill(); strokeWeight(3.25); stroke(color("#387BBF")); 
@@ -115,11 +112,12 @@ function draw(){
   for (var i = 0; i < 15; i++) {
     ellipse((xcanvas*xover+boxwidth+3.5-esize)-(shifted*i),ycanvas*eycanvas,esize,esize);
   }
+  fill(color("#387BBF")); strokeWeight(3.25); stroke(color("#387BBF"));
+  ellipse((xcanvas*xover+boxwidth+3.5-esize)-(shifted*mr),ycanvas*eycanvas,esize,esize);
   pop();
 
   // State Variables
-  var mr = 0; var chosenMR = multiracialMatrix[mr];
-  var cs = 1;
+  var chosenMR = multiracialMatrix[mr];
 
   // Top Title Page
   textAlign(LEFT);
@@ -137,14 +135,122 @@ function draw(){
 
   fill(255);
   text("are ", topicLinex+((chosenMR.length)*linespace), topicLiney);
-  text("to live in these", topicLinex+((chosenMR.length)+(are.length)+(chooseSeverity[cs].length))*linespace, topicLiney);
+  text("to live in these", topicLinex+((chosenMR.length)+(are.length)+(chooseSeverity[cs].length)-1)*linespace, topicLiney);
   text("counties", topicLinex, topicLiney+explaintxt);
+  pop();
+
+  // charts
+  var diameter = 400;
+
+  noStroke();
+  var lastAngle = 0;
+  var leftx = xcanvas/2; rightx = leftx*3;
+  var topy = ycanvas/1.5; bottomy = topy*2.2;
+  var smallcircle = 300;
+  for (var i = 0; i < angles.length; i++) {
+    fill(color(gray[i]));
+    arc(leftx/2, topy/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
+    lastAngle += radians(angles[i]);
+    fill(color('#152F48'));
+    ellipse(leftx/2, topy/2, smallcircle, smallcircle);
+  }
+  for (var i = 0; i < angles.length; i++) {
+    fill(color(gray[i]));
+    arc(leftx/2, bottomy/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
+    lastAngle += radians(angles[i]);
+    fill(color('#152F48'));
+    ellipse(leftx/2, bottomy/2, smallcircle, smallcircle);
+  }
+  for (var i = 0; i < angles.length; i++) {
+    fill(color(gray[i]));
+    arc(rightx/2, topy/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
+    lastAngle += radians(angles[i]);
+    fill(color('#152F48'));
+    ellipse(rightx/2, topy/2, smallcircle, smallcircle);
+  }
+  for (var i = 0; i < angles.length; i++) {
+    fill(color(gray[i]));
+    arc(rightx/2, bottomy/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
+    lastAngle += radians(angles[i]);
+    fill(color('#152F48'));
+    ellipse(rightx/2, bottomy/2, smallcircle, smallcircle);
+  }
+
+
+  push();
+  var leftx = xcanvas/4; rightx = leftx*3;
+  var topy = ycanvas/3; bottomy = topy*2.2;
+  textStyle(BOLD); fill(255);
+  textAlign(CENTER);
+  var txtsize = 80; var smlltxtsz = 22;
+  var yoffset = 15; var racoffset = 30; var boffset = racoffset+smlltxtsz+10; 
+  // top left
+  textSize(txtsize); text("2%", leftx,topy-yoffset);
+  textSize(smlltxtsz); text(multiracialMatrix[0], leftx,topy+racoffset); text(multiracialNumber[0],leftx,topy+boffset); 
+  // top right
+  textSize(txtsize); text("2%", rightx,topy-yoffset);
+  textSize(smlltxtsz); text(multiracialMatrix[0], rightx,topy+racoffset); text(multiracialNumber[0],rightx,topy+boffset); 
+  // bottom right
+  textSize(txtsize); text("2%", rightx,bottomy-yoffset);
+  textSize(smlltxtsz); text(multiracialMatrix[0], rightx,bottomy+racoffset); text(multiracialNumber[0],rightx,bottomy+boffset); 
+  // bottom left
+  textSize(txtsize); text("2%", leftx,bottomy-yoffset);
+  textSize(smlltxtsz); text(multiracialMatrix[0], leftx,bottomy+racoffset); text(multiracialNumber[0],leftx,bottomy+boffset); 
+  pop();
+
+  push();
+  var leftx = xcanvas/4; rightx = leftx*3;
+  var topy = ycanvas/3; bottomy = topy*2.2; var yoffset = 25;
+  textSize(30), textAlign(CENTER); textStyle(BOLD); fill(color("#387BBF"));
+  text("County 1"+" State", leftx, topy-(diameter/2)-yoffset);
+  text("County 2"+" State", leftx, bottomy-(diameter/2)-yoffset);
+  text("County 3"+" State", rightx, topy-(diameter/2)-yoffset);
+  text("County 4"+" State", rightx, bottomy-(diameter/2)-yoffset);
   pop();
 
 }
 
 // 500 should be the larger outer circle when you hover over!!!
 
+
+// ------------------------------FUNCTIONS for INTERACTION---------------------------------
+
+
+function mousePressed() {
+
+  var boxheight = 45, boxwidth = 142, xover = .785, yover = .93;
+  if (mouseX > xcanvas*xover && mouseX < xcanvas*xover+boxwidth && mouseY > ycanvas*yover && mouseY < (ycanvas*yover)+boxheight) {
+    status = 2;
+    console.log("State is 2, least likely!")
+    cs =2; 
+  } 
+
+  if (mouseX > xcanvas*xover-boxwidth-20 && mouseX < xcanvas*xover-boxwidth-20+boxwidth && mouseY > ycanvas*yover && mouseY < (ycanvas*yover)+boxheight){
+    status = 1;
+    console.log("State is 1, median!")
+    cs = 1;
+  }
+  else if(mouseX > xcanvas*xover-(boxwidth*2)-40 && mouseX < xcanvas*xover-(boxwidth*2)-40+boxwidth && mouseY > ycanvas*yover && mouseY < (ycanvas*yover)+boxheight){
+    status = 0;
+    console.log("State is 0, most")
+    cs = 0;
+  }
+
+  //change the MultiRacial Paring 
+  var ebeg = xcanvas*xover+boxwidth+3.5-esize; 
+  for (var i = 0; i <15; i++) {
+    if( mouseY > ycanvas*eycanvas && mouseY < ycanvas*eycanvas+esize && mouseX > (xcanvas*xover+boxwidth+3.5-esize)-(shifted*i)-esize && mouseX < (xcanvas*xover+boxwidth+3.5-esize)-(shifted*i)){
+
+    fill(color("#387BBF")); strokeWeight(3.25); stroke(color("#387BBF"));
+    ellipse((xcanvas*xover+boxwidth+3.5-esize)-(shifted*i),ycanvas*eycanvas,esize,esize);
+    mr = i;
+    console.log(mr)
+  }
+  }
+
+}
+
+/*
 function pieChart(diameter, data) {
   noStroke();
   var lastAngle = 0;
@@ -212,43 +318,7 @@ function pieChart(diameter, data) {
   text("County 4"+" State", rightx, bottomy-(diameter/2)-yoffset);
   pop();
 }
-
-// ------------------------------FUNCTIONS for INTERACTION---------------------------------
-
-
-function mousePressed() {
-
-  var boxheight = 45, boxwidth = 142, xover = .785, yover = .93;
- // rect(xcanvas*xover, ycanvas*yover, boxwidth, boxheight, 11);
-  //rect(xcanvas*xover-boxwidth-20, ycanvas*yover, boxwidth, boxheight, 11);
-  //rect(xcanvas*xover-(boxwidth*2)-40, ycanvas*yover, boxwidth, boxheight, 11);
-
-  if (mouseX > xcanvas*xover && mouseX < xcanvas*xover+boxwidth && mouseY > ycanvas*yover && mouseY < (ycanvas*yover)+boxheight) {
-    status = 2;
-    console.log("State is 2, least likely!")
-  } 
-
-  if (mouseX > xcanvas*xover-boxwidth-20 && mouseX < xcanvas*xover-boxwidth-20+boxwidth && mouseY > ycanvas*yover && mouseY < (ycanvas*yover)+boxheight){
-    status = 1;
-    console.log("State is 1, median!")
-  }
-  else if(mouseX > xcanvas*xover-(boxwidth*2)-40 && mouseX < xcanvas*xover-(boxwidth*2)-40+boxwidth && mouseY > ycanvas*yover && mouseY < (ycanvas*yover)+boxheight){
-    status = 0;
-    console.log("State is 0, most")
-  }
-
-  //change the MultiRacial Paring 
-  var ebeg = xcanvas*xover+boxwidth+3.5-esize; 
-  if( mouseY > ycanvas*eycanvas && mouseY < ycanvas*eycanvas+esize ){
-
-    mr = 0;
-
-    for (var i = 0; i < 15; i++) {
-    ellipse((xcanvas*xover+boxwidth+3.5-esize)-(shifted*i),ycanvas*eycanvas,esize,esize);
-  }
-  }
-
-}
+*/
 
 
 
